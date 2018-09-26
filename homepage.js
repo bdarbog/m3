d3e1cf43128ff17628152f35012d8b4214deedf2
bdarbog <bdarbog@bdarbog.it.pointpark.edu>
2018-09-26 16:15:42 -0400

    second commit

--- homepage.js
+++ homepage.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var fortune = require("./fortune");
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res, next){
+        res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+        next();
+});
+
+
 app.get('/', function(req, res) {
         res.render('home');
 });
-app.get('/about', function(req, res){
-        res.render('about');
+app.get('/about', function(req, res){ 
+        res.render('about', {
+	fortune: fortune.getFortune(),
+	pageTestScript: '/qa/tests-about.js'
+});
 });
 app.get('/datetime', function(req, res) {
         res.render('datetime');
